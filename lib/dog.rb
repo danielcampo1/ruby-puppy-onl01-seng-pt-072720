2d08dcb0f4c0264526410e56d2be3c442018070f
daniel campo <campo_daniel94@yahoo.com>
2020-08-18 19:00:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
-# Add your code here
+class Dog
+  
+  @@all = []
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+  @name = name 
+  
+end
+  
+  def self.all
+    @@all 
+  end
+  def print_all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  
+end

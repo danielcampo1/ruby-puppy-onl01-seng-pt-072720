d739bfa0f55fa1ed1cdf80e693ccf24e7b74aaac
daniel campo <campo_daniel94@yahoo.com>
2020-08-18 19:08:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,30 @@
-class Dog
-  
-  attr_accessor :name
-  
+class Dog 
   @@all = []
   
-  def initialize(name)
-    @name= name
-    @@all << self
-  save
-  end
+  attr_accessor :name 
   
   def save
     @@all << self
-  end
+  end   
+  
+  def initialize(name)
+    self.save 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.print_all
+    @@all.each do |puppy|
+      puts puppy.name 
+    end   
+  end   
+  
+  def self.clear_all
+    @@all.clear 
+  end   
+  
 end
 
 pluto = Dog.new
